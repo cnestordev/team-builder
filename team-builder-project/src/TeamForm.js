a5bf71ec842c05ea452deffbd764b2f1d2f20ea5
Nestor <ncervan87@gmail.com>
2020-07-21 15:19:01 -0700

    minor refactoring

--- team-builder-project/src/TeamForm.js
+++ team-builder-project/src/TeamForm.js
@@ -3,10 +3,14 @@ import React from 'react'
 function TeamForm(props) {
     return (
         <div className="form-container">
-            <form>
+            <form className="form-main">
+                <label htmlFor="name">Name</label>
                 <input onChange={props.handler} value={props.formData.name} type="text" name="name" placeholder="first name" id="name" />
+
+                <label htmlFor="email">Email</label>
                 <input onChange={props.handler} value={props.formData.email} type="email" name="email" placeholder="Email" id="email" />
-                <label htmlFor="cars">Choose a car:</label>
+
+                <label htmlFor="cars">Choose a role:</label>
                 <select onChange={props.handler} value={props.formData.role} id="role" name="role">
                     <option value="Frontend">Frontend</option>
                     <option value="Backend">Backend</option>
